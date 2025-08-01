cb4e74d1314124090e980fbf5d4d6adc210223be
GofarP <gofarperdana83@gmail.com>
2025-08-01 10:52:01 +0700

    Use location (Done)

--- src/DataLayout.jsx
+++ src/DataLayout.jsx
@@ -1,16 +1,41 @@
-import { Outlet } from "react-router";
-
+import { NavLink, Outlet, useLocation } from "react-router";
+import './data.css'
 export default function DataLayout() {
-    return(
+    const location = useLocation();
+
+    return (
         <>
             <div>
                 <h1>This is header</h1>
             </div>
             <div>
-                <Outlet/>
+                <Outlet />
             </div>
             <div>
-                <p>This is footer</p>
+                <ul>
+                    <li>
+                        <NavLink to="/data/products">Products</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/data/sellers">Sellers</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/data/customers">Customers</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={{
+                            pathname: "/data/products",
+                            search: "?category=shoes",
+                            hash: "#top"
+                        }} >Products
+                        </NavLink>
+                    </li>
+                </ul>
+
+            </div>
+            <div>
+                <h3>This is footer</h3>
+                <p>Location:{location.pathname} {location.search} {location.hash}</p>
             </div>
         </>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,8 @@ import DataLayout from './DataLayout.jsx'
 import Data from './Data.jsx'
 import ProductDetail from './ProductDetail.jsx'
 import Image from './Image.jsx'
+import NotFound from './NotFound.jsx'
+import ProductSearch from './ProductSearch.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
@@ -22,10 +24,12 @@ createRoot(document.getElementById('root')).render(
         <Route path='/data' element={<DataLayout />}>
           <Route index element={<Data />} />
           <Route path="products" element={<Product />} />
+          <Route path="products/search" element={<ProductSearch />} />
           <Route path="products/:id" element={<ProductDetail/>} />
           <Route path="customers" element={<Customer />} />
           <Route path="sellers" element={<Seller />} />
         </Route>
+        <Route path='/*' element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
